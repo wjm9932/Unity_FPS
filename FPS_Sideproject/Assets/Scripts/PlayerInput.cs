6a4a9a5df9029958709030537beafc1abedc2fe1
Jaemin <wjm9932@gmail.com>
2023-12-07 11:12:46 -0800

    Implement player Movement

---
+++ FPS_Sideproject/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 moveInput { get; private set; }
+    public bool isJump { get; private set; }
+    public bool isFire { get; private set; }
+    //public bool isReload { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (moveInput.sqrMagnitude > 1f)
+        {
+            moveInput = moveInput.normalized;
+        }
+        isJump = Input.GetButtonDown("Jump");
+        isFire = Input.GetButton("Fire1");
+        //isReload = Input.GetButtonDown("Reload");
+    }
+}
---
+++ FPS_Sideproject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+
+    private CharacterController playerController;
+    private PlayerInput input;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        input = GetComponent<PlayerInput>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded == true && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        Vector3 moveDir = transform.forward * input.moveInput.y + transform.right * input.moveInput.x;   
+        playerController.Move(moveDir * Speed * Time.deltaTime);
+
+        if(input.isJump == true && isGrounded == true)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity*Time.deltaTime);
+    }
+}
